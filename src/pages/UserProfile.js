95fd350984bf427de56881e7b969e2e9d5a10d51
SinaParvaresh <60529544+SinaParvaresh@users.noreply.github.com>
2023-01-18 13:11:12 -0600

    User Profile - Added input fields to User Profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,16 +1,35 @@
+import { Button, Container, Grid, Title } from "@mantine/core";
 import React, { Fragment } from "react";
-import { Card } from "@mantine/core";
 import { HeaderMegaMenu } from "../components/Layout/HeaderMegaMenu";
 import { FloatingLabelInput } from "../components/UI/FloatingLabelInput";
+// import FormCard from "../components/UI/FormCard";
 
 const UserProfile = (props) => {
   return (
     <Fragment>
       <HeaderMegaMenu />
-      <Card>
-        <FloatingLabelInput label="First Name" placeholder="First Name" />
-        <FloatingLabelInput label="Last Name" placeholder="Last Name" />
-      </Card>
+      <Container>
+        <form>
+          <Title order={2} m="md">
+            Account Details
+          </Title>
+
+          <Grid justify="center">
+            <Grid.Col span={6}>
+              <FloatingLabelInput label="First Name" placeholder="First Name" />
+            </Grid.Col>
+
+            <Grid.Col span={6}>
+              <FloatingLabelInput label="Last Name" placeholder="Last Name" />
+            </Grid.Col>
+
+            <Grid.Col>
+              <FloatingLabelInput label="Email" placeholder="Email" />
+            </Grid.Col>
+            <Button>Update Information</Button>
+          </Grid>
+        </form>
+      </Container>
     </Fragment>
   );
 };
